87f17cb8a6f12a256068f2ad17e39d07bfdb3734
Marco <markosayajin@hotmail.com>
2021-09-22 19:11:36 -0500

    First Test

--- login/lambda_function.py
+++ login/lambda_function.py
@@ -1,1 +1,13 @@
-from flask import Flask;
+import json
+
+def hello_world(name):
+    test = 'Hello World'
+    return test
+
+def lambda_handler(event, context):
+    test = hello_world(event['name'])
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!'),
+        'response': json.dumps(test)
+    }
