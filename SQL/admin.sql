0b9bf729cc844c1f17cb3ca829a6218a271dd37e
Marco <markosayajin@hotmail.com>
2021-09-22 19:57:51 -0500

    User Table
    sql table user

--- SQL/admin.sql
+++ SQL/admin.sql
@@ -1,3 +1,14 @@
+Create Table If Not Exists Usuario(
+	idUsuario VARCHAR(255) NOT NULL,
+	nombre VARCHAR(255) NOT NULL,
+	apellidoM VARCHAR(255) NOT NULL,
+	apellidoP VARCHAR(255) NOT NULL,
+	correo VARCHAR(255) NOT NULL,
+	contrasena VARCHAR(255) NOT NULL,
+	estatusHoras INT NOT NULL,
+	ubicacion VARCHAR(255) NOT NULL,
+	PRIMARY KEY(idUsuario)
+);
 CREATE TABLE Admin(id varchar(255)  NOT NULL,
  					  contrasena varchar(255) NOT NULL,
  					  PRIMARY KEY (id),
