fa8ee6c0f49d74c35c83be198b8de1310296398d
Marco <markosayajin@hotmail.com>
2021-09-22 20:22:24 -0500

    Services Table
    create Services Table

--- SQL/admin.sql
+++ SQL/admin.sql
@@ -17,7 +17,24 @@ Create Table If Not Exists Documentos(
     PRIMARY KEY(idUser),
 	FOREIGN KEY(idUser) REFERENCES Usuario(idUsuario)
 );
-CREATE TABLE Admin(id varchar(255)  NOT NULL,
+Create Table If Not Exists Servicios(
+    idServicio VARCHAR(255) NOT NULL,
+    idUsuario  VARCHAR(255) NOT NULL,
+    ubicacion  VARCHAR(255) NOT NULL,
+    categoria  VARCHAR(255) NOT NULL,
+    nombre     VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    tiempoActivo TIME NOT NULL,
+    V0 INT NOT NULL,
+    V1 INT NOT NULL,
+    V2 INT NOT NULL,
+    V3 INT NOT NULL,
+    V4 INT NOT NULL,
+    V5 INT NOT NULL,
+    PRIMARY KEY(idServicio),
+    FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario)
+);
+CREATE TABLE If Not Exists Admin(id varchar(255)  NOT NULL,
  					  contrasena varchar(255) NOT NULL,
  					  PRIMARY KEY (id),
  				    );
