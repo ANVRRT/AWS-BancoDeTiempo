b66ab7fad233bca78f9951ea11ee6a2b62d0d8e0
ANVRRT <albertonavarreteramirez@gmail.com>
2021-09-28 00:16:52 -0500

    Up to date with lambda, login function

--- AWS-Lambda/Functions/login/lambda_function.py
+++ AWS-Lambda/Functions/login/lambda_function.py
@@ -7,19 +7,15 @@ def approve_login(user,password):
     dbHandler = DBC()
     dbHandler.SQL_initialize()
 
-    password = password.encode()
-    password = sha256(password).hexdigest()
+    # password = password.encode()
+    # password = sha256(password).hexdigest()
 
     query = f"SELECT * FROM Usuario WHERE idUsuario = \"{user}\" AND contrasena = \"{password}\""
 
     queryResult = dbHandler.SQL_execute_twoway_statement(query)
 
     dbHandler.SQL_stop()
-    
-    
-    
 
-    
     if queryResult:
         queryResult = queryResult[0]
         approval = 1
@@ -39,8 +35,6 @@ def approve_login(user,password):
             }
     
     return data
-    
-    #return 1
 
 def lambda_handler(event, context):
     
