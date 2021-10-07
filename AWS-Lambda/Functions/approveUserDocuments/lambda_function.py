b8236289da42f16f368cf9cb88abdc521bb002d2
ANVRRT <albertonavarreteramirez@gmail.com>
2021-10-07 14:41:26 -0500

    Finished approveUserDocuments functionalities

--- AWS-Lambda/Functions/approveUserDocuments/lambda_function.py
+++ AWS-Lambda/Functions/approveUserDocuments/lambda_function.py
@@ -1,34 +1,74 @@
 import json
 from dbc import DBC
 
-def approve_all(username):
-    pass
 
-def approve_ine(username):
-    pass
+def define_query(type,username):
+    query = ""
+    if type == "INE":
+        query = f"UPDATE Documentos SET ine_approval = 1 WHERE idUser = \"{username}\""
+    
+    if type == "Comprobante":
+        query = f"UPDATE Documentos SET comprobante_approval = 1 WHERE idUser = \"{username}\""
 
-def approve_comprobante(username):
-    pass
+    if type == "Carta":
+        query = f"UPDATE Documentos SET cartaAntecedentes_approval = 1 WHERE idUser = \"{username}\""
 
-def approve_carta(username):
-    pass
+    return query
 
-import json
-from dbc import DBC
+def approve_all(dbHandler, username):
+    query = f"UPDATE Documentos SET ine_approval = 1, comprobante_approval = 1, cartaAntecedentes_approval = 1 WHERE idUser = \"{username}\""
+    queryResult = dbHandler.SQL_execute_oneway_statement(query)
+    if not queryResult:
+        return False
+
+    query2 = f"UPDATE Usuario SET documentos_approval = 1, estatusHoras = 1 WHERE idUsuario = \"{username}\""
+    queryResult2 = dbHandler.SQL_execute_oneway_statement(query2)
+    if not queryResult2:
+        return False
+
+    return True
+
 
 def lambda_handler(event, context):
-    
-    id1 = event['queryStringParameters']['username']
-    id2 = event['queryStringParameters']['type']
+    """
+    "All"
+    "INE"
+    "Comprobante"
+    "Carta"
+
+    """
+    username = event['queryStringParameters']['username']
+    type = event['queryStringParameters']['type']
+
+    dbHandler = DBC()
+    dbHandler.SQL_initialize()
+
+    if type == "All":
+        approval = approve_all(dbHandler, username)
+        if approval:
+            approval = 1
+        else:
+            approval = 0
+
+    else:
+        query = define_query(type, username)
+        queryResult = dbHandler.SQL_execute_oneway_statement(query)
+        if not queryResult:
+            approval = 0
+        else:
+            approval = 1
 
     transaction = {}
     
-    transaction["id"] = id1
-    transaction["type"] =id2
-
-    responseObject = {}
+    transaction["transactionApproval"] = approval    
     
-    responseObject['statusCode'] = 200
-    responseObject['body'] = json.dumps(transaction)
-    
-    return responseObject
+    data = {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        },
+        'body': json.dumps(transaction)
+    }
+    return data
