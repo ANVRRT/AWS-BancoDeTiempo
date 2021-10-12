7b22d3bd5b5cd341a72299a31a03847bcf328a46
ANVRRT <albertonavarreteramirez@gmail.com>
2021-10-11 20:53:40 -0500

    Modified for a single location

--- AWS-Lambda/Functions/getServices/lambda_function.py
+++ AWS-Lambda/Functions/getServices/lambda_function.py
@@ -1,27 +1,12 @@
 import boto3
 from dbc import DBC
 
-def generatePreQuery(colonias, categoria):
-    preQuery = ""
-
-    for colonia in colonias:
-        preSecQuery = f"ubicacion = \"{colonia}\" OR "
-        preQuery += preSecQuery
-
-    preQuery = preQuery[:-3]
-    preQuery += f"AND categoria = \"{categoria}\" AND estado = 1"
-
-    return preQuery
-
-
-def get_services(colonias, category):
+def get_services(colonia, category):
 
     dbHandler = DBC()
     dbHandler.SQL_initialize()
 
-    preQuery = generatePreQuery(colonias, category)
-
-    query = f"SELECT * FROM Servicios WHERE {preQuery}"
+    query = f"SELECT * FROM Servicios WHERE ubicacion = \"{colonia}\" AND categoria = \"{category}\" AND estado = 1 "
 
     # query = f"SELECT * FROM Recibe LEFT JOIN Servicios ON ubicacion = \"{colonia}\" AND estado = \"OPEN\" "
     queryResult = dbHandler.SQL_execute_twoway_statement(query)
@@ -30,13 +15,6 @@ def get_services(colonias, category):
     data = {}
 
     for service in queryResult:
-        # data[service[0]] = {
-        #                     "idCita": service[0],
-        #                     "idReceptor": service[1],
-        #                     "idServicio": service[2],
-        #                     "idEmisor": service[3],
-        #                     "comentario": service[5],
-        #                     }
         data[service[0]] = {
                     "idServicio": service[0],
                     "idUsuario": service[1],
@@ -51,14 +29,10 @@ def get_services(colonias, category):
 
 def lambda_handler(event, context):
 
-    colonias = event["colonia"]
+    colonia = event["colonia"]
     category = event["categoria"]
-    coloniasArray = []
 
-    for colonia in colonias.keys():
-        coloniasArray.append(colonia)
-
-    data = get_services(coloniasArray, category)
+    data = get_services(colonia, category)
     
 
     return data
