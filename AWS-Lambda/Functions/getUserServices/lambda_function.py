696af88ffc4fe2eda3ad74c5691ba00bc2456fd9
ANVRRT <albertonavarreteramirez@gmail.com>
2021-10-11 21:51:55 -0500

    Added get user services

--- AWS-Lambda/Functions/getUserServices/lambda_function.py
+++ AWS-Lambda/Functions/getUserServices/lambda_function.py
@@ -1,17 +1,15 @@
 import boto3
 from dbc import DBC
 
-def get_services(colonia, category):
+def get_user_services(username):
 
     dbHandler = DBC()
     dbHandler.SQL_initialize()
 
-    query = f"SELECT * FROM Servicios WHERE ubicacion = \"{colonia}\" AND categoria = \"{category}\" AND estado = 1 "
+    query = f"SELECT * FROM Servicios WHERE idUsuario = \"{username}\" "
 
-    # query = f"SELECT * FROM Recibe LEFT JOIN Servicios ON ubicacion = \"{colonia}\" AND estado = \"OPEN\" "
     queryResult = dbHandler.SQL_execute_twoway_statement(query)
 
-    # data = query
     data = {"ofertas": []}
 
     for service in queryResult:
@@ -30,10 +28,9 @@ def get_services(colonia, category):
 
 def lambda_handler(event, context):
 
-    colonia = event["colonia"]
-    category = event["categoria"]
+    username = event["username"]
 
-    data = get_services(colonia, category)
+    data = get_user_services(username)
     
 
     return data
