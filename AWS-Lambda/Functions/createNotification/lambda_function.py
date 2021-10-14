3fda2f690f23b0c93dfcd2dfc9e4e8d92a8ab76b
ANVRRT <albertonavarreteramirez@gmail.com>
2021-10-14 02:17:46 -0500

    Added 'ACCEPTED' 'REQUEST' notification services

--- AWS-Lambda/Functions/createNotification/lambda_function.py
+++ AWS-Lambda/Functions/createNotification/lambda_function.py
@@ -1,5 +1,119 @@
 from dbc import DBC
 
+def check_active_service(dbHandler ,username, userType):
+
+    if userType == "Emisor":
+        query = f"SELECT estado FROM Recibe WHERE idEmisor = \"{username}\" AND estado = \"OPEN\" "
+
+    elif userType == "Receptor":
+        query = f"SELECT estado FROM Recibe WHERE idReceptor = \"{username}\" AND estado = \"OPEN\" "
+
+    queryResult = dbHandler.SQL_execute_twoway_statement(query)
+    if queryResult:
+        return True
+
+    return False
+
+def check_active_hours(dbHandler, username):
+
+    query = f"SELECT estatusHoras FROM Usuario WHERE idUsuario = \"{username}\" AND estatusHoras = 1 "
+    queryResult = dbHandler.SQL_execute_twoway_statement(query)
+    
+    if not queryResult:
+        return False
+    return True
+
+def remove_active_hours(dbHandler, username):
+
+    query = f"UPDATE Usuario SET estatusHoras = 0 WHERE idUsuario = \"{username}\""
+    queryResult = dbHandler.SQL_execute_oneway_statement(query)
+
+def deactivate_REQUEST_notifications(dbHandler, username, userType):
+
+    if userType == "Emisor":
+        query = f"UPDATE Notificacion SET estado = 0 WHERE idEmisor = \"{username}\" AND tipo = \"REQUEST\" "
+    elif userType == "Receptor":
+        query = f"UPDATE Notificacion SET estado = 0 WHERE idReceptor = \"{username}\" AND tipo = \"REQUEST\" "
+    
+    queryResult = dbHandler.SQL_execute_oneway_statement(query)
+
+def deactivate_services(dbHandler, username):
+
+    query = f"UPDATE Servicios SET estado = 0 WHERE idUsuario = \"{username}\" "
+    queryResult = dbHandler.SQL_execute_oneway_statement(query)
+
+def activate_service(dbHandler, idReceptor, idEmisor, idServicio):
+    query = f"""INSERT INTO Recibe VALUES(
+            NULL,
+            \"{idReceptor}\",
+            \"{idServicio}\",
+            \"{idEmisor}\",
+            \"OPEN\"
+            )
+            """
+    queryResult = dbHandler.SQL_execute_oneway_statement(query)
+    
+# def update_notification_to_accepted(dbHandler, idNot):
+#     query = f"UPDATE Notification SET tipo = \"ACCEPTED\" WHERE idNotificacion = {idNot}"
+#     queryResult = dbHandler.SQL_execute_oneway_statement(query)
+
+
+def preprocess_transaction(dbHandler, data):
+
+    if data["tipo"] == "REQUEST":
+
+        activeHours = check_active_hours(dbHandler,data["idReceptor"])
+        if not activeHours:
+            return False
+
+        activeService = check_active_service(dbHandler, data['idEmisor'], "Emisor")
+        if activeService:
+            return False
+
+    elif data["tipo"] == "ACCEPTED":
+
+
+        #Check hours of the one that wants the service
+        activeHours = check_active_hours(dbHandler,data["idReceptor"])
+        if not activeHours:
+            return False
+
+        #Check if no service is currently going on the one that wants the service
+        activeService = check_active_service(dbHandler, data['idReceptor'], "Receptor")
+        if activeService:
+            return False
+
+        #Check if the one that is giving the service has no other currently going on services.
+        activeService = check_active_service(dbHandler, data['idEmisor'], "Emisor")
+        if activeService:
+            return False
+
+
+        #Delete hours of the one that wants the service
+        remove_active_hours(dbHandler, data['idReceptor'])
+
+
+        #Deactivate all REQUEST notifications of the one that wants the service
+        deactivate_REQUEST_notifications(dbHandler, data['idReceptor'], "Receptor")
+
+        #Deactivate all REQUEST notifications of the one that is giving the service
+        deactivate_REQUEST_notifications(dbHandler, data['idEmisor'], "Emisor")
+
+
+        #Deactivate all services of the one that that wants the service
+        deactivate_services(dbHandler, data['idReceptor'])
+
+        #Deactivate all services of the one that is giving the service 
+        deactivate_services(dbHandler, data['idEmisor'])
+
+        #Generate registry of Recibe table setting an "OPEN" service for the 2 actors.
+        activate_service(dbHandler, data["idReceptor"], data["idEmisor"], data["idServicio"])
+
+        #Update REQUEST notification to ACCEPTED and activate it again
+        #It gets done in the main defined query
+        
+
+    return True
 
 def define_query(data):
 
@@ -8,8 +122,9 @@ def define_query(data):
                     NULL,
                     \"{data["idEmisor"]}\",
                     \"{data["idReceptor"]}\",
-                    \"{data["idServicio"]}\",
-                    \"{data["tipo"]}\"
+                    {data["idServicio"]},
+                    \"{data["tipo"]}\",
+                    1
                     )
                     """
     else:
@@ -24,19 +139,25 @@ def process_notification(dataReceived):
     dbHandler = DBC()
     dbHandler.SQL_initialize()
 
-    query = define_query(dataReceived)
-
-    transactionApproval = 1
-
-    queryResult = dbHandler.SQL_execute_oneway_statement(query)
-
-    if not queryResult:
-        transactionApproval = 0
-
     data = {
-            "transactionApproval": transactionApproval
-            }
+        "transactionApproval": 1
+        }
 
+    approval = preprocess_transaction(dbHandler, dataReceived)
+
+    if approval:
+
+        query = define_query(dataReceived)
+
+        queryResult = dbHandler.SQL_execute_oneway_statement(query)
+
+        if not queryResult:
+            data["transactionApproval"] = 0
+    else:
+
+        data["transactionApproval"] = 0
+        
+        
     return data
 
 def lambda_handler(event, context):
@@ -49,7 +170,16 @@ def lambda_handler(event, context):
                 "idReceptor": event["idReceptor"],
                 "tipo": event["type"]
                 }
+                
+    elif event["type"] == "ACCEPTED":
 
+        data = {
+                "idServicio": event["idServicio"],
+                "idEmisor": event["idEmisor"],
+                "idReceptor": event["idReceptor"],
+                "tipo": event["type"],
+                "idNot": event["idNot"]
+                }
     else:
 
         data = {
@@ -58,8 +188,6 @@ def lambda_handler(event, context):
                 }
 
     
-
-
     data = process_notification(data)
     
 
---
+++ AWS-Lambda/Functions/getNotifications/lambda_function.py
@@ -0,0 +1,47 @@
+from dbc import DBC
+
+def getNotifications(username):
+
+    dbHandler = DBC()
+    dbHandler.SQL_initialize()
+
+    query = f"""SELECT idNotificacion, idEmisor, idReceptor, idServicio, tipo, nombre, descripcion FROM Notificacion INNER JOIN Servicios 
+                    ON Servicios.idServicio = Notificacion.idServicio 
+                    WHERE (idEmisor = \"{username}\" AND tipo = \"REQUEST\") AND estado = 1
+
+                    UNION
+
+                SELECT idNotificacion, idEmisor, idReceptor, idServicio, tipo, nombre, descripcion FROM Notificacion INNER JOIN Servicios 
+                    ON Servicios.idServicio = Notificacion.idServicio 
+                    WHERE (idReceptor = \"{username}\" AND (tipo = \"ACCEPTED\" OR tipo = \"REJECTED\") ) AND estado = 1
+                    """
+
+    queryResult = dbHandler.SQL_execute_twoway_statement(query)
+
+    data = {"notificaciones": []}
+
+    for service in queryResult:
+        
+        data["notificaciones"].append({
+                    "idNot": service[0],
+                    "idEmisor": service[1],
+                    "idReceptor": service[2],
+                    "idServicio": service[3],
+                    "tipo": service[4],
+                    "nombre": service[5],
+                    "descripcion": service[6]
+                    })
+
+    return data
+
+def lambda_handler(event, context):
+
+    username = event["username"]
+
+    
+
+
+    data = getNotifications(username)
+    
+
+    return data
