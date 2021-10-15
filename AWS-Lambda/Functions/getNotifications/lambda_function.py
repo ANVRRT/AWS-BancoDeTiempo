3fe70e1e2d67b6b2d052d0473138c1868b225037
ANVRRT <albertonavarreteramirez@gmail.com>
2021-10-14 20:24:35 -0500

    Added 'WAITING' and 'CONTACTING' notifications

--- AWS-Lambda/Functions/getNotifications/lambda_function.py
+++ AWS-Lambda/Functions/getNotifications/lambda_function.py
@@ -31,6 +31,43 @@ def getNotifications(username):
                     "nombre": service[5],
                     "descripcion": service[6]
                     })
+
+    query2 = f"""SELECT idNotificacion, idEmisor, idReceptor, Notificacion.idServicio, tipo, nombre, descripcion FROM Notificacion INNER JOIN Servicios 
+                    ON Servicios.idServicio = Notificacion.idServicio 
+                    WHERE (idReceptor = \"{username}\" AND tipo = \"REQUEST\")  AND Notificacion.estado = 1
+            """
+    queryResult2 = dbHandler.SQL_execute_twoway_statement(query2)
+
+    for service in queryResult2:
+        
+        data["notificaciones"].append({
+                    "idNot": service[0],
+                    "idEmisor": service[1],
+                    "idReceptor": service[2],
+                    "idServicio": service[3],
+                    "tipo": "WAITING",
+                    "nombre": service[5],
+                    "descripcion": service[6]
+                    })
+    query3 = f"""SELECT idNotificacion, idEmisor, idReceptor, Notificacion.idServicio, tipo, nombre, descripcion FROM Notificacion INNER JOIN Servicios 
+                    ON Servicios.idServicio = Notificacion.idServicio 
+                    WHERE (idEmisor = \"{username}\" AND tipo = \"ACCEPTED\") AND Notificacion.estado = 1
+                """
+
+    queryResult3 = dbHandler.SQL_execute_twoway_statement(query3)
+
+    for service in queryResult3:
+        
+        data["notificaciones"].append({
+                    "idNot": service[0],
+                    "idEmisor": service[1],
+                    "idReceptor": service[2],
+                    "idServicio": service[3],
+                    "tipo": "CONTACTING",
+                    "nombre": service[5],
+                    "descripcion": service[6]
+                    })
+
                     
     dbHandler.SQL_stop()
 
